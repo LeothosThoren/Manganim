4711e2ffd2ab79b302e5f8fc0f7d3eab2a04c053
Leothos Thoren <soussou.med@live.fr>
2025-10-22 21:03:00 +0200

    chore: Update target and compile SDK versions to 36, and upgrade Android Gradle Plugin to 8.13.0

--- build-logic/convention/src/main/kotlin/SdkVersions.kt
+++ build-logic/convention/src/main/kotlin/SdkVersions.kt
@@ -1,5 +1,5 @@
 object SdkVersions {
     const val MIN_SDK = 26 // Ajustez selon vos besoins
-    const val TARGET_SDK = 35
-    const val COMPILE_SDK = 35
+    const val TARGET_SDK = 36
+    const val COMPILE_SDK = 36
 }
